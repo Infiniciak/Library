a5c9430a11db736f842eae32d6808f67dbcaba0e
Bartoszw <infiniciak@gmail.com>
2025-05-08 23:44:30 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+using System;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
---
+++ Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<Borrow> Borrows { get; set; } = new List<Borrow>();
+    }
+}
